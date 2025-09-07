981a7d90d2284eabe72916c94f39e9ce7a481dc1
Rios Silvas <riosilvas827@gmail.com>
2025-09-07 04:27:24 +0000

    make it reflow well there overflow in pop up  (_for element <Primitive.d

--- src/components/dashboard/welcome-modal.tsx
+++ src/components/dashboard/welcome-modal.tsx
@@ -52,7 +52,7 @@ export function WelcomeModal() {
         <div className="grid gap-4 py-4">
             <p>You can either jump right into a new calculation or explore a sample dashboard with pre-filled data to see what's possible.</p>
         </div>
-        <div className="flex flex-col sm:flex-row gap-2">
+        <div className="flex flex-col gap-2 sm:flex-row">
             <Button onClick={handleStart} className="w-full gradient-primary">
                 <Rocket className="mr-2 h-4 w-4" /> Start My First Calculation
             </Button>
